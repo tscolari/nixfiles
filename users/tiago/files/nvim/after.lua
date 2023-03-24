1c37497287cc1c2f7da4c06254165de7beb52c00
Tiago Scolari <git@tscolari.me>
2023-03-24 11:04:32 +0000

    Remove custom vim configuration
    This is now in the nvim config repo

--- users/tiago/files/nvim/after.lua
+++ users/tiago/files/nvim/after.lua
@@ -3,47 +3,12 @@ vim.cmd [[
 " Called after everything just before setting a default colorscheme
 " Configure you own bindings or other preferences. e.g.:
 
-" Disable swap files please {{{
-set noswapfile
-set nobackup
-set nowb
-" }}}
-
-" Create window splits easier. The default
-" way is Ctrl-w,v and Ctrl-w,s. I remap
-" this to vv and ss {{{
-nnoremap <silent> vv <C-w>v
-nnoremap <silent> ss <C-w>s
-" }}}
-
-" Spelling extras {{{
-setlocal spelllang=en_us
-set spell spelllang=en_us
-
-iab requrie require
-iab emtpy empty
-iab emtpy? empty?
-iab intereset interest
-iab proeprty property
-
-autocmd BufRead,BufNewFile *.md setlocal spell
-autocmd BufRead,BufNewFile *.rdoc setlocal spell
-autocmd BufRead,BufNewFile *.markdown setlocal spell
-autocmd FileType gitcommit setlocal spell
-" }}}
-
-" style {{{
-colorscheme gruvbox
-" }}}
 
 " no folds {{{
 set nofoldenable
 set foldlevelstart=9999
 " }}}
 
-" Switch between test and production code {{{
-nnoremap <leader>. :GoAlt<cr>
-" }}}
 
 " Set the -local flag to the current Go module
 autocmd FileType go let b:go_fmt_options = {
@@ -51,6 +16,4 @@ autocmd FileType go let b:go_fmt_options = {
       \ trim(system('{cd '. shellescape(expand('%:h')) .' && go list -m;}')),
       \ }
 
-" Change the behaviour of the paste to not replace the copy when pasting in visual mode.
-vnoremap p "_dP
 ]]
--- users/tiago/files/nvim/before.lua
+++ users/tiago/files/nvim/before.lua
@@ -1,29 +1,4 @@
 vim.cmd [[
 
-" Called before everything, even before loading plugins
-" Do things that need to happen very early such as:
-" let g:fzf_command_prefix = 'Fuzzy'
-" ...
-
-
-""" FocusMode
-function! ToggleFocusMode()
-  if (&foldcolumn != 12)
-    set laststatus=0
-    set numberwidth=10
-    set foldcolumn=12
-    set noruler
-    hi FoldColumn ctermbg=none
-    hi LineNr ctermfg=0 ctermbg=none
-    hi NonText ctermfg=0
-  else
-    set laststatus=2
-    set numberwidth=4
-    set foldcolumn=0
-    set ruler
-    execute 'colorscheme ' . g:colors_name
-  endif
-endfunc
-nnoremap <F1> :call ToggleFocusMode()<cr>
 
 ]]
