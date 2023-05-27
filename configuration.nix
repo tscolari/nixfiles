588bfd9715c8c9c54ea0fd33bfb7964bbd1959fa
Tiago Scolari <git@tscolari.me>
2023-05-27 09:50:32 +0100

    Set caps as control always

--- configuration.nix
+++ configuration.nix
@@ -51,8 +51,11 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver = {
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+    xkbOptions = "caps:ctrl_modifier";
+  };
 
   services.flatpak.enable = true;
 
