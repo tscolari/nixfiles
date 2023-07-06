bd3170a7c99b649555c7197daaa6e39c9b230a31
Tiago Scolari <git@tscolari.me>
2023-07-06 10:39:11 +0100

    Move gnome packages to gnome.nix

--- home/common/gnome.nix
+++ home/common/gnome.nix
@@ -10,6 +10,37 @@ in {
     file = {
       "${homeDir}/.background.jpg".source = ../files/background.jpg;
     };
+
+    packages = [
+    # Gnome extensions
+      pkgs.gnomeExtensions.clipboard-indicator
+      pkgs.gnomeExtensions.dash-to-panel
+      pkgs.gnomeExtensions.openweather
+      pkgs.gnomeExtensions.screenshot-window-sizer
+      pkgs.gnomeExtensions.sound-output-device-chooser
+      pkgs.gnomeExtensions.tray-icons-reloaded
+      pkgs.gnomeExtensions.user-themes
+      pkgs.gnomeExtensions.windownavigator
+      pkgs.gnomeExtensions.custom-hot-corners-extended
+      pkgs.gnomeExtensions.vitals
+      pkgs.gnomeExtensions.stocks-extension
+      pkgs.gnomeExtensions.night-theme-switcher
+
+    # Gtk themes / icons
+      pkgs.flat-remix-gnome
+      pkgs.flat-remix-gtk
+      pkgs.flat-remix-icon-theme
+      pkgs.numix-cursor-theme
+      pkgs.numix-gtk-theme
+      pkgs.numix-icon-theme
+      pkgs.papirus-icon-theme
+      pkgs.arc-icon-theme
+      pkgs.arc-theme
+      pkgs.yaru-theme
+
+    # Misc
+      pkgs.gnome.gnome-tweaks
+    ];
   };
 
   gtk = {
@@ -58,7 +89,7 @@ in {
         "dash-to-dock@micxgx.gmail.com"
         "sound-output-device-chooser@kgshank.net"
         "custom-hot-corners-extended@G-dH.github.com"
-        # "Vitals@CoreCoding.com"
+        "quick@web.search"
         "nightthemeswitcher@romainvigier.fr"
         # "material-shell@papyelgringo"
       ];
--- home/common/packages.nix
+++ home/common/packages.nix
@@ -8,63 +8,33 @@ in {
 
   home = {
     # Packages that should be installed to the user profile.
-      packages = [
-       # pkgs.git-duet
-       # pkgs.jsonpp
-       # pkgs.pyenv
+    packages = [
+     # pkgs.git-duet
+     # pkgs.jsonpp
+     # pkgs.pyenv
 
-      # Misc
-        pkgs.shortwave
-        pkgs._1password-gui
-        pkgs.chromium
-        pkgs.spotify
-        pkgs.transmission
+    # Misc
+      pkgs.shortwave
+      pkgs._1password-gui
+      pkgs.chromium
+      pkgs.spotify
+      pkgs.transmission
 
-      # Game
-        pkgs.steam
+    # Game
+      pkgs.steam
 
-      # Code
-        pkgs.xclip
-        unstable.alacritty
+    # Code
+      pkgs.xclip
+      unstable.alacritty
 
-      # Chat
-        pkgs.slack
-        pkgs.discord
+    # Chat
+      pkgs.slack
+      pkgs.discord
 
-      # Video
-        pkgs.zoom-us
-        pkgs.skypeforlinux
-
-      # Gnome extensions
-        pkgs.gnomeExtensions.clipboard-indicator
-        pkgs.gnomeExtensions.dash-to-panel
-        pkgs.gnomeExtensions.openweather
-        pkgs.gnomeExtensions.screenshot-window-sizer
-        pkgs.gnomeExtensions.sound-output-device-chooser
-        pkgs.gnomeExtensions.tray-icons-reloaded
-        pkgs.gnomeExtensions.user-themes
-        pkgs.gnomeExtensions.windownavigator
-        pkgs.gnomeExtensions.custom-hot-corners-extended
-        pkgs.gnomeExtensions.vitals
-        pkgs.gnomeExtensions.stocks-extension
-        pkgs.gnomeExtensions.night-theme-switcher
-        pkgs.gnomeExtensions.material-shell
-
-      # Gtk themes / icons
-        pkgs.flat-remix-gnome
-        pkgs.flat-remix-gtk
-        pkgs.flat-remix-icon-theme
-        pkgs.numix-cursor-theme
-        pkgs.numix-gtk-theme
-        pkgs.numix-icon-theme
-        pkgs.papirus-icon-theme
-        pkgs.arc-icon-theme
-        pkgs.arc-theme
-        pkgs.yaru-theme
-
-      # Gnome apps
-        pkgs.gnome.gnome-tweaks
-      ];
+    # Video
+      pkgs.zoom-us
+      pkgs.skypeforlinux
+    ];
   };
 
   programs.fzf = {
