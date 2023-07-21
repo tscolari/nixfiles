e1a9ef2567bbd54278bae8aa114bd606fd893153
Tiago Scolari <git@tscolari.me>
2023-07-21 20:33:01 +0100

    Move dellxps configuration out

--- configuration.nix
+++ configuration.nix
@@ -21,8 +21,6 @@ in {
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  boot.kernelParams = ["psmouse.synaptics_intertouch=0"];
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
--- machines/dellxps-hardware-configuration.nix
+++ machines/dellxps-hardware-configuration.nix
@@ -37,4 +37,7 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.kernelParams = ["psmouse.synaptics_intertouch=0"];
 }
