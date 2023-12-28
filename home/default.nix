1d21f74527853dce34165e57fe81a415182fb763
Tiago Scolari <git@tscolari.me>
2023-12-28 20:38:15 +0000

    Bump to 23.11

--- home/default.nix
+++ home/default.nix
@@ -55,7 +55,7 @@ in {
       # You can update Home Manager without changing this value. See
       # the Home Manager release notes for a list of state version
       # changes in each release.
-      stateVersion = "23.05";
+      stateVersion = "23.11";
 
       # Files and remote configurations
     };
