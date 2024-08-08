a9ba2265f455c8ed91f370a3470676c399f1214d
Tiago Scolari <git@tscolari.me>
2024-08-08 14:06:07 +0100

    Bump nixos state version

--- configuration.nix
+++ configuration.nix
@@ -109,7 +109,7 @@ in {
 
   services.flatpak.enable = true;
 
-  programs.nix-ld.dev.enable = true;
+  programs.nix-ld.dev.enable = false;
 
   programs.zsh.enable = true;
 
@@ -289,13 +289,17 @@ in {
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  networking.hosts = {
+    "116.202.198.11" = [ "homelab" ];
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "24.05"; # Did you read the comment?
 
   # Experimental features
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
--- home/darwin.nix
+++ home/darwin.nix
@@ -59,7 +59,7 @@ in {
       # You can update Home Manager without changing this value. See
       # the Home Manager release notes for a list of state version
       # changes in each release.
-      stateVersion = "23.11";
+      stateVersion = "24.05";
 
       # Files and remote configurations
     };
