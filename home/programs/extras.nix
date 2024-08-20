5ad74118d856192e317444e5f371b78a4ecacc00
Tiago Scolari <git@tscolari.me>
2024-08-20 22:21:54 +0100

    Add OBS

--- home/programs/extras.nix
+++ home/programs/extras.nix
@@ -31,5 +31,15 @@ in {
     # Video
       pkgs.skypeforlinux
     ];
+
+  };
+
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+    ];
   };
 }
