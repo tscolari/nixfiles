ee47a3d902236389ac9ca8b3c68f39be3f59a3ac
Tiago Scolari <git@tscolari.me>
2025-04-20 09:07:39 +0100

    Add custom firefox shortcuts for work

--- home-manager/by-user/work/default.nix
+++ home-manager/by-user/work/default.nix
@@ -8,6 +8,8 @@ in {
   home = {
     file = {
       "${homeDir}/.config/zsh/p10k.zsh".source = ./files/p10k.zsh;
+      "${homeDir}/.local/share/applications/work-firefox.desktop".source = ./files/work-firefox.desktop;
+      "${homeDir}/.local/share/applications/personal-firefox.desktop".source = ./files/personal-firefox.desktop;
     };
   };
 }
