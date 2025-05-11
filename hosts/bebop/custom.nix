7a7e5c2bde6e22effd7432bb752796e528485d57
Tiago Scolari <git@tscolari.me>
2025-05-11 08:07:32 +0100

    Pin kernel 6.12 on bebop

--- hosts/bebop/custom.nix
+++ hosts/bebop/custom.nix
@@ -1,5 +1,6 @@
 { config, nixos-hardware, lib, pkgs, modulesPath, ... }:
 {
+  boot.kernelPackages = pkgs.linuxPackages_6_12;
   boot.kernelModules = ["kvm-intel"];
   hardware.enableAllFirmware = true;
 }
