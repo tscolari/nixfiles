3f22cdd4ef81cc2de99263d74ab438a53f3fe823
Tiago Scolari <git@tscolari.me>
2025-10-10 14:42:49 +0100

    Neovim lsp/lang migration

--- home-manager/programs/nvim/autocmds.nix
+++ home-manager/programs/nvim/autocmds.nix
@@ -21,12 +21,43 @@
         };
         desc = "Don't format when adding lines with o/O";
       }
+
       {
         event = "FocusGained";
         group = "config#basic";
         command = "checktime";
         desc = "Reload file on focus";
       }
+
+      {
+        event = "FileType";
+        pattern = [ "yaml" ];
+        desc = "Disable diagnostics for YAML files (can't handle Helm templates)";
+        callback = {
+          __raw = ''
+            function(args)
+              -- Disable diagnostics in the buffer
+              vim.diagnostic.disable(args.buf)
+
+              -- Disable CursorHold diagnostic popup (like Lspsaga hover diagnostics)
+              vim.api.nvim_clear_autocmds({
+                  event = "CursorHold",
+                  buffer = args.buf,
+                  })
+
+            -- Safely clear Lspsaga autocommands if group exists
+              local ok, group_autocmds = pcall(vim.api.nvim_get_autocmds, { group = "LspsagaAutocmds" })
+              if ok and group_autocmds and #group_autocmds > 0 then
+                vim.api.nvim_clear_autocmds({
+                    group = "LspsagaAutocmds",
+                    buffer = args.buf,
+                })
+              end
+            end
+          '';
+        };
+      }
+
     ];
   };
 }
