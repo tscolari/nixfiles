4a28cef9470948a5fa0192a4b942cd19196bddc8
Tiago Scolari <tiago.scolari@redpanda.com>
2025-10-31 10:43:28 +0000

    Add virtualbox

--- hosts/TARS/default.nix
+++ hosts/TARS/default.nix
@@ -7,4 +7,8 @@
     ./networking.nix
   ];
 
+  virtualisation.virtualbox.host.enable = true;
+  # virtualisation.virtualbox.host.enableExtensionPack = true;
+  virtualisation.virtualbox.guest.enable = true;
+  virtualisation.virtualbox.guest.dragAndDrop = true;
 }
